a0a605b9db9fb1bc572fd0df5e0203ce210b6035
AWD9999 <keefs969@gmail.com>
2024-11-15 17:19:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// Функция для отправки выбранного класса боту
-function chooseClass(className) {
-    // Отправка данных через Telegram Web Apps API
-    window.Telegram.WebApp.sendData(className);
-    alert(`Вы выбрали класс: ${className}`);
+function chooseAction(action) {
+    // Отправляем выбранное действие в Telegram Web App
+    window.Telegram.WebApp.sendData(action);
 }
-
-// Другие возможные функции для обработки интерфейса Mini App можно добавить здесь
